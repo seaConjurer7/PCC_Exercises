a3649e2a42288ed0c4e412ca7b25625d7805b770
seaConjurer7 <sweetatious27@gmail.com>
2019-03-25 22:22:23 -0700

    03.25.2019

--- chapter_8.py
+++ chapter_8.py
@@ -1,6 +1,8 @@
 # A small program for practicing concepts learned in chapter 8 
 # Carsen Weinzetl 03.19.2019
 
+# ------------------------------------------- #
+
 # Making a function that greets people
 def greet_user():
 	'''Display a simple greeting'''
@@ -8,6 +10,8 @@ def greet_user():
 
 # Calling the function
 greet_user()
+
+# ------------------------------------------- #
 
 # Making a function that calls someone with a parameter
 def greet_user(username):
@@ -17,6 +21,8 @@ def greet_user(username):
 # Calling the function with a parameter
 greet_user('Jesse')
 
+# ------------------------------------------- #
+
 # Making a function that prints a message taking two parameters
 def pets(animal_type, pet_name):
 	print('\nI have a ' + animal_type + '.')
@@ -25,6 +31,8 @@ def pets(animal_type, pet_name):
 # Calling the function twice, with different parameters
 pets('Dog', 'Spaghetti')
 pets('Cat', 'Pumpernickel')
+
+# ------------------------------------------- #
 
 # Remaking the function above with keyword arguments 
 # to ensure no mixup in the order of inputs
@@ -36,5 +44,9 @@ def describe_pet(animal_type, pet_name):
 # Calling the function ensuring that the order no longer matters
 describe_pet(animal_type='Hamster', pet_name='Bob')
 describe_pet(pet_name='Bob',animal_type='Hamster')
+
+# ------------------------------------------- #
+
+
 
 
