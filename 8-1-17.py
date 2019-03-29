5766c31bfd1f41cf7989a38ffc18403fd6454765
seaConjurer7 <sweetatious27@gmail.com>
2019-03-28 22:53:31 -0700

    03.28.2019

--- 8-1-17.py
+++ 8-1-17.py
@@ -1,6 +1,8 @@
 # A program for exercises in chapter 8
 # Carsen Weinzetl 03.19.2019
 
+# ------------------------------------------- #
+
 # Writing a function called display_message() that prints one sentence
 # telling everyone what you are learning about in this chapter. Call the function
 # to make sure it works properly 
@@ -10,10 +12,33 @@ def display_message():
 # Calling the function
 display_message()
 
+# ------------------------------------------- #
+
 # Writting a function that prints a personalized message about someones favorite
 # book, then call the function to make sure it works. 
 def fav_book(book):
-	print('My favorite book is, ' + book + '!')
+	print('\nMy favorite book is, ' + book + '!')
 
 # Calling the function 
 fav_book('"A Time to Kill"')
+
+# ------------------------------------------- #
+
+# Writing a function that accepts a t-shirt size, and the text of a message
+# that should be printed on the t-shirt. The function should then print a summary
+# of the tshirt itself
+def tshirt(size, message):
+	print('\nThe shirt is a size, ' + size.lower() + '. That says: ' + message.title())
+
+# Calling the function
+tshirt(
+	size = 'Large',
+	message = 'Theres no speed limit in heaven.'
+)
+
+# ------------------------------------------- #
+
+
+
+
+
