aab3d367cae899296b5695777db8f64f45ee149c
seaConjurer7 <sweetatious27@gmail.com>
2019-03-28 23:23:53 -0700

    03.28.2019

--- 8-1-17.py
+++ 8-1-17.py
@@ -27,18 +27,43 @@ fav_book('"A Time to Kill"')
 # Writing a function that accepts a t-shirt size, and the text of a message
 # that should be printed on the t-shirt. The function should then print a summary
 # of the tshirt itself
-def tshirt(size, message):
-	print('\nThe shirt is a size, ' + size.lower() + '. That says: ' + message.title())
+print('\n')
 
-# Calling the function
+def tshirt(size, message):
+	print('The shirt is a size, ' + size.lower() + 
+	'. That says: ' + message.title())
+
+# Calling the function with positional & keyword arguments
+tshirt('Large', 'There is no speed limit in heaven.')
+
 tshirt(
 	size = 'Large',
-	message = 'Theres no speed limit in heaven.'
+	message = 'There is no speed limit in heaven.'
 )
 
 # ------------------------------------------- #
 
+# Rewritting the function above with default messages
+def tshirt2():
+	print('\nThe shirt is a size, Large. With a message that says: "I love Python!".')
 
+# Calling the function 
+tshirt2()
 
+# ------------------------------------------- #
+
+# Writting a function that accepts the name of a city and its country. 
+# It should print a simple sentence using the two arguments given. 
+print('\n')
+
+def places(city): 
+	print(city.title() + ' is in the USA.')
+
+# Calling the function three times, with one of cities not matching the statement
+places('Tampa')
+places('Boston')
+places('Ontario')
+
+# ------------------------------------------- #
 
 
